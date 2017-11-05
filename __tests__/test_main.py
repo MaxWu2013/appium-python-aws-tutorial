80ec319e985c42dfcda4c04e93d3d911ea27faec
HL <hi.heyse@gmail.com>
2017-11-05 17:29:24 -0500

    Fix failing test

--- __tests__/test_main.py
+++ __tests__/test_main.py
@@ -1,9 +1,7 @@
 def test_basic(driver):
-    WAIT_SEC = 20
+    WAIT_SEC = 10
     driver.implicitly_wait(WAIT_SEC)
 
     elem = driver.find_element_by_accessibility_id('testview')
-    not_there = driver.find_element_by_accessibility_id('notthere')
 
     assert elem is not None
-    assert not_there is None
