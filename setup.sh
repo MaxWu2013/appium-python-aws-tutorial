040d0a7efb0145b29ac5fd9a9e447c72b06a964c
HL <hi.heyse@gmail.com>
2018-01-24 17:38:32 -0500

    Exit venv after install

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ yarn
 virtualenv venv
 source venv/bin/activate # Enter virtual environment
 pip install -r requirements.txt # Install python dependencies to virtual env
-deactivate
+deactivate # Exit virtual env
