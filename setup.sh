6990a24235c750dab9d9d07b08640853611d7bf5
hyperh <hyperh@users.noreply.github.com>
2018-01-24 17:45:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 yarn
 
 # Python
-virtualenv venv
+virtualenv venv # Create virtual environment folder
 source venv/bin/activate # Enter virtual environment
 pip install -r requirements.txt # Install python dependencies to virtual env
 deactivate # Exit virtual env
