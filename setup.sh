256630f51297037487d0604822850a5ad799be1e
HL <hi.heyse@gmail.com>
2018-01-24 17:38:09 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Install JS dependencies
+yarn
+
+# Python
+virtualenv venv
+source venv/bin/activate # Enter virtual environment
+pip install -r requirements.txt # Install python dependencies to virtual env
+deactivate
